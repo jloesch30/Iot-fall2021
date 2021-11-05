bdeac6509010c5b77d409d2a4d6d298862e8476e
jloesch30 <61180397+jloesch30@users.noreply.github.com>
2021-11-05 12:04:58 -0500

    Setting up MS3

---
+++ ms1/a1.py
@@ -0,0 +1,6 @@
+
+"""
+This is the code for A1, microbit
+"""
+
+# import microbit librarj
---
+++ ms3/SampleMQTTAndroidApp-master/app/src/main/java/edu/utexas/mpc/samplemqttandroidapp/MainActivity.kt
@@ -0,0 +1,82 @@
+package edu.utexas.mpc.samplemqttandroidapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
+import org.eclipse.paho.client.mqttv3.MqttMessage
+
+class MainActivity : AppCompatActivity() {
+
+    // I'm using lateinit for these widgets because I read that repeated calls to findViewById
+    // are energy intensive
+    lateinit var textView: TextView
+    lateinit var syncButton: Button
+
+    // I'm doing a late init here because I need this to be an instance variable but I don't
+    // have all the info I need to initialize it yet
+    lateinit var mqttAndroidClient: MqttAndroidClient
+
+    // you may need to change this depending on where your MQTT broker is running
+    val serverUri = "tcp://192.168.4.6:1883"
+    // you can use whatever name you want to here
+    val clientId = "EmergingTechMQTTClient"
+
+    //these should "match" the topics on the "other side" (i.e, on the Raspberry Pi)
+    val subscribeTopic = "testTopic2"
+    val publishTopic = "testTopic1"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = this.findViewById(R.id.text)
+        syncButton = this.findViewById(R.id.syncButton)
+
+        // when the user presses the syncbutton, this method will get called
+        syncButton.setOnClickListener({ syncWithPi() })
+
+        // initialize the paho mqtt client with the uri and client id
+        mqttAndroidClient = MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+
+        // when things happen in the mqtt client, these callbacks will be called
+        mqttAndroidClient.setCallback(object: MqttCallbackExtended {
+
+            // when the client is successfully connected to the broker, this method gets called
+            override fun connectComplete(reconnect: Boolean, serverURI: String?) {
+                println("Connection Complete!!")
+                // this subscribes the client to the subscribe topic
+                mqttAndroidClient.subscribe(subscribeTopic, 0)
+                val message = MqttMessage()
+                message.payload = ("Hello World").toByteArray()
+
+                // this publishes a message to the publish topic
+                mqttAndroidClient.publish(publishTopic, message)
+            }
+
+            // this method is called when a message is received that fulfills a subscription
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                println(message)
+            }
+
+            override fun connectionLost(cause: Throwable?) {
+                println("Connection Lost")
+            }
+
+            // this method is called when the client succcessfully publishes to the broker
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                println("Delivery Complete")
+            }
+        })
+
+    }
+
+    // this method just connects the paho mqtt client to the broker
+    fun syncWithPi(){
+        println("+++++++ Connecting...")
+        mqttAndroidClient.connect()
+    }
+
+}
